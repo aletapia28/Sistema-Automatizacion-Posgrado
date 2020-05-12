87529e55c36c1fbddbfbbdda099bb3dda5a16ee5
aletapia28 <aletapiab28@gmail.com>
2020-05-11 18:42:21 -0600

    recuperando datos fila

--- Sistema-Automatizacion/src/app/vtn-editar-asistente/vtn-editar-asistente.component.ts
+++ Sistema-Automatizacion/src/app/vtn-editar-asistente/vtn-editar-asistente.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-
 @Component({
   selector: 'app-vtn-editar-asistente',
   templateUrl: './vtn-editar-asistente.component.html',
