3d72c1686068e103a555ef06af34691217468e9b
aletapia28 <aletapiab28@gmail.com>
2020-05-29 18:57:32 -0600

    FIxed ,

--- Sistema-Automatizacion/src/app/app.module.ts
+++ Sistema-Automatizacion/src/app/app.module.ts
@@ -85,7 +85,7 @@ const routes: Routes = [
     VtnEditarAsistenteComponent,
     ConfirmDialogComponent,
     VtnBuscarPostulanteComponent,
-    DescargarMemoComponent
+    DescargarMemoComponent,
     VtnEditarFormulaComponent
   ],
   imports: [
