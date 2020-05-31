366cc0f26270215c369c745adbe2db89f5948455
Casasola98 <jgomezcasasola@gmail.com>
2020-05-31 15:21:59 -0600

    Validar espacios bien

--- Sistema-Automatizacion/src/app/vtn-editar-asistente/vtn-editar-asistente.component.ts
+++ Sistema-Automatizacion/src/app/vtn-editar-asistente/vtn-editar-asistente.component.ts
@@ -40,9 +40,11 @@ export class VtnEditarAsistenteComponent implements OnInit {
   onSubmit() {
     let correopr = sessionStorage.getItem('correoAsistente');
 
-    let nombr:string = this.editarAForm.get('nombre').value.replace(/\s/g, "");
+    let nombr:string = this.editarAForm.get('nombre').value;
     let ced:string = this.editarAForm.get('cedula').value.replace(/\s/g, "");
     let newpass:string = this.editarAForm.get('passwd').value.replace(/\s/g, "");
+
+    nombr = nombr.trim();
 
     if ((correopr.length > 0) && (newpass.length > 0) && (ced.length > 0) && (nombr.length > 0)) {
       const formData = { correo: correopr, password: newpass, nombre: nombr, cedula: ced }
