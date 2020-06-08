e7324f55ff257b6421ab82878802d17ae42833f5
aletapia28 <aletapiab28@gmail.com>
2020-06-08 17:54:28 -0600

    form recuperar password

--- Sistema-Automatizacion/src/app/vtn-recuperar-pass/vtn-recuperar-pass.component.ts
+++ Sistema-Automatizacion/src/app/vtn-recuperar-pass/vtn-recuperar-pass.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit,Inject } from '@angular/core';
+import { FormGroup, FormControl, Validators, NgForm, FormGroupDirective } from '@angular/forms';
+import { NotificationService } from '../shared/notification.service';
 
 import {MAT_DIALOG_DATA,MatDialogRef} from '@angular/material/dialog';
 
@@ -8,6 +10,10 @@ import {MAT_DIALOG_DATA,MatDialogRef} from '@angular/material/dialog';
   styleUrls: ['./vtn-recuperar-pass.component.css']
 })
 export class VtnRecuperarPassComponent implements OnInit {
+  
+  Passform = new FormGroup({
+    correo: new FormControl('', [Validators.required, Validators.email]),
+  });
 
   constructor(
     @Inject (MAT_DIALOG_DATA) public data,
@@ -24,7 +30,8 @@ export class VtnRecuperarPassComponent implements OnInit {
   }
 
   enviar(){
-    console.log("ENviar Correo");
+    let correnv: string = this.Passform.get('correo').value.replace(/\s/g, "");
+    console.log(correnv);
   }
 
 }
