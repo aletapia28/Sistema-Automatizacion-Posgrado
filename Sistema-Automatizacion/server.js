acdb8dd78c4bcc57c9047b09abfc28678438a693
aletapia28 <aletapia28@hotmail.com>
2020-09-02 20:39:12 -0600

    Front end serve

--- Sistema-Automatizacion/server.js
+++ Sistema-Automatizacion/server.js
@@ -5,6 +5,9 @@ var bodyParser = require("body-parser")
 var app = express()
 var port = process.env.PORT || 3000
 
+const path = require('path');
+const fs = require('fs');
+
 //extrae el json del http dl request 
 app.use(bodyParser.json())
 app.use(cors())
@@ -12,10 +15,24 @@ app.use(
     bodyParser.urlencoded({extended:false})
 )
 
-//
 var Routes = require("./routes/Routes.js")
 app.use("/router",Routes)
 
-app.listen(port,function(){
-    console.log("Server is running on port" + port)
-})
+
+const root = path.join(__dirname, 'dist', 'Sistema-Automatizacion');
+
+
+app.get('*' ,function(req, res) {
+  fs.stat(root + req.path, function(err){
+    if(err){
+        res.sendFile("index.html", { root });
+    }else{
+        res.sendFile(req.path, { root });
+    }
+  })
+});
+
+app.listen(port);
+console.log('Listening on port '+ port);
+
+
